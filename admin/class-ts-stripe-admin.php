7476cc31e4fab9b432f47b724b00bc902606dc9d
Leia Sefkin <leiasefkin@Uncorkeds-MBP-7.(none)>
2018-03-30 15:27:18 -0700

    cleanup since these aren't needed

--- admin/class-ts-stripe-admin.php
+++ admin/class-ts-stripe-admin.php
@@ -55,28 +55,6 @@ class TS_Stripe_Admin {
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/ts-stripe-admin.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ts-stripe-admin.js', array( 'jquery' ), $this->version, false );
-
-	}
-
-	/**
 	 * Register the administration menu for this plugin into the WordPress Dashboard menu.
 	 *
 	 * @since    1.0.0
