97e1617018646ad7cde4f0438f70f529f57dbbf4
Leia Sefkin <leiasefkin@Uncorkeds-MBP-7.(none)>
2018-03-31 15:57:06 -0700

    initial db creation code

--- includes/class-ts-stripe-activator.php
+++ includes/class-ts-stripe-activator.php
@@ -23,14 +23,47 @@
 class TS_Stripe_Activator {
 
 	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
+	 * Basic activation
 	 *
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+		self::ts_stripe_db_install();
 		do_action( 'ts_stripe_activated' );
+	}
+
+
+	/**
+	 * Create database tables.
+	 * These store basic information around donations.
+	 * Time donation was made, name, email, amount, card last 4.
+	 * 
+	 * @since    1.0.0
+	 */
+	public static function ts_stripe_db_install() {
+		global $wpdb;
+		global $ts_stripe_db_version;
+
+		$ts_stripe_install = '1.0';
+		$table_name = $wpdb->prefix . 'ts_stripe';
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE IF NOT EXISTS $table_name (
+		  id mediumint(9) NOT NULL AUTO_INCREMENT,
+		  time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		  name varchar(255) DEFAULT '' NOT NULL,
+		  email varchar(255) DEFAULT '' NOT NULL,
+		  last4 int(5) NOT NULL,
+		  amount int(10) NOT NULL,
+		  PRIMARY KEY  (id)
+		) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+		echo $wpdb->last_error;
+
+		add_option( 'ts_stripe_db_version', $ts_stripe_db_version );
+
 	}
 
 }
